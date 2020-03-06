c2161636dea3d8817d862453368e411dd79dbbbf
Elliot Alderson <blackman_5007@hotmail.com>
2020-03-06 17:46:26 +0100

    Add project files.

---
+++ Basics/Program.cs
@@ -0,0 +1,29 @@
+﻿using Amazon.Runtime;
+using Amazon.S3;
+using System;
+using System.Configuration;
+
+namespace Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //aws hesabına bağlanılacak
+            var credentials = new BasicAWSCredentials(
+                ConfigurationManager.AppSettings["accessId"],
+                ConfigurationManager.AppSettings["secretKey"]);
+
+            using (AmazonS3Client client = new AmazonS3Client
+                (credentials, Amazon.RegionEndpoint.EUWest1))
+            {
+                foreach (var item in client.ListBuckets().Buckets)
+                {
+                    Console.WriteLine(item.BucketName + " " +
+                        item.CreationDate.ToShortDateString());
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ S3Console/Program.cs
@@ -0,0 +1,17 @@
+﻿
+namespace S3Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var operation = new S3BucketOprations();
+            operation.UploadFile();
+            operation.DownloadFileAsync().Wait();
+            operation.PresSignedUrl();
+            operation.GetObjectTagging();
+            operation.UpdateObjectTagging();
+
+        }
+    }
+}
